5b7b9afc1b9870f8dac10bd85ea5eb1a4ad5238d
ixiumu <ixiumu@users.noreply.github.com>
2024-06-25 00:27:41 +0800

    token

--- .github/script/setup.sh
+++ .github/script/setup.sh
@@ -38,24 +38,7 @@ else
     echo "No SSH key found for user: $GITHUB_ACTOR"
     echo "Setting SSH password..."
 
-    #
-    # This command could be "beautifully" lazy like the commented line below,
-    # but for some reason on GitHub Actions specifically (and nowhere else I
-    # could find) it logs the following warnings:
-    #
-    #     tr: write error: Broken pipe
-    #     tr: write error
-    #     base64: write error: Broken pipe
-    #     base64: write error
-    #
-    # Even though it still succeeds and behaves properly, I'd rather not have
-    # those warnings show up, so I use `head` twice, getting 16 bytes off
-    # `/dev/urandom`, Base64-encoding it (which should give us 25 characters),
-    # and we drop all the nonalphanumeric ones and trim it to 16 chars.
-    #
     password=$(head -c 16 /dev/urandom | base64 | tr -cd '[:alnum:]' | head -c 16)
-    # password=$(base64 < /dev/urandom | tr -cd '[:alnum:]' | head -c16)
-
     (echo "$password"; echo "$password") | sudo passwd "$USER"
 fi
 
@@ -76,77 +59,8 @@ echo 'Starting tmux session...'
 
 # Use `sed -u` (unbuffered) otherwise logs don't show up in the UI
 echo 'Starting Cloudflare tunnel...'
-./cloudflared tunnel --no-autoupdate --url tcp://localhost:2222 2>&1 | tee cloudflared.log | sed -u 's/^/cloudflared: /' &
+./cloudflared tunnel --no-autoupdate run --token "$CLOUDFLARED_TUNNEL_TOKEN" 2>&1 | tee cloudflared.log | sed -u 's/^/cloudflared: /' &
 cloudflared_pid=$!
-
-#
-# Tail `cloudflared.log` to find the part where they share the relay
-# hostname.
-#
-# Shell substitution `<()` required to prevent the pipeline from hanging
-# even after it finds a first match. See <https://stackoverflow.com/a/45327054>.
-#
-# Requires GNU Bash.
-#
-url=$(head -1 <(tail -f cloudflared.log | grep --line-buffered -o 'https://.*\.trycloudflare.com'))
-
-# POSIX-compatible but just hangs
-# url=$(tail -f cloudflared.log | grep --line-buffered -o 'https://.*\.trycloudflare.com' | head -1)
-
-# POSIX-compatible using simple polling instead
-# url=$(while ! grep -o 'https://.*\.trycloudflare.com' cloudflared.log; do sleep 1; done)
-
-# Ignore the `user@host` part at the end of the public key
-public_key=$(cut -d' ' -f1,2 < ssh_host_rsa_key.pub)
-
-# Notify the actor and output to the run log
-
-(
-    # Echo spaces on empty lines because if we just echo a newline, GitHub will eat it
-    echo '    '
-    echo '    '
-    echo '    '
-    echo '    '
-    echo 'Run the following command to connect:'
-    echo '    '
-    echo "    ssh-keygen -R action-sshd-cloudflared && echo 'action-sshd-cloudflared $public_key' >> ~/.ssh/known_hosts && ssh -o ProxyCommand='cloudflared access tcp --hostname $url' runner@action-sshd-cloudflared"
-
-    #
-    # Unlike Upterm that runs a custom SSH server to accept passwords in the
-    # username field with the `:password` syntax, standard `sshd` doesn't
-    # accept that, so we need to paste the password like mere mortals.
-    #
-    if [ -n "$password" ]; then
-        echo '    '
-        echo "    # Password: $password"
-    fi
-
-    #
-    # You might notice we use `action-sshd-cloudflared` as a SSH host to connect.
-    # This is abritrary and we could put anything here, because of the
-    # `ProxyCommand` option later, the host is ignored and we directly go through
-    # the tunnel exposed by `cloudflared`. But for the `ssh` command to be valid,
-    # we still need to give it a host.
-    #
-    echo '    '
-    echo "What the one-liner does:"
-    echo '    '
-    echo '    # Remove old SSH server public key for `action-sshd-cloudflared`'
-    echo "    ssh-keygen -R action-sshd-cloudflared"
-    echo '    '
-    echo '    # Trust the public key for this session'
-    echo "    echo 'action-sshd-cloudflared $public_key' >> ~/.ssh/known_hosts"
-    echo '    '
-    echo '    # Connect using `cloudflared` as a transport (SSH is end-to-end encrpted over this tunnel)'
-    echo "    ssh -o ProxyCommand='cloudflared access tcp --hostname $url' runner@action-sshd-cloudflared"
-    echo '    '
-    echo "    # Alternative if you don't want to verify the host key"
-    echo "    ssh -o ProxyCommand='cloudflared access tcp --hostname $url' -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=accept-new runner@action-sshd-cloudflared"
-    echo '    '
-    echo '    '
-    echo '    '
-    echo '    '
-) | notify_func
 
 #
 # The `channel` argument is not used but needs to be passed.
