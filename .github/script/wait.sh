e1767ef69f29f4fed35100175a6ecb2d6ab06077
ixiumu <ixiumu@users.noreply.github.com>
2024-06-25 02:55:19 +0800

    test

--- .github/script/wait.sh
+++ .github/script/wait.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set script timeout (minutes)
-timeout=1
+timeout=30
 
 # Convert timeout to seconds
 timeout_seconds=$((timeout * 60))
@@ -14,7 +14,7 @@ while true; do
   # Get the current number of active SSH user connections
   #current_user_count=$(who | grep -c "pts/")
   current_user_count=$(ps x | grep sshd | grep -c @)
-  
+
   echo "Active user connections: ${current_user_count}"
 
   if [ $current_user_count -gt 0 ]; then
